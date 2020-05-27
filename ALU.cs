4d1c073608563cc17c2eb324e2fb2a9b06733583
MonicaTanios <monicatanios@gmail.com>
2020-05-27 15:58:02 +0200

    Add ALU Classes

---
+++ ALU.cs
@@ -0,0 +1,23 @@
+﻿namespace MipsEmulator
+{
+    static class ALU
+    {
+        public static uint ValueOne { get; set; }
+        public static uint ValueTwo { get; set; }
+        public static uint Result { get; set; }
+        public static string Operation { get; set; }
+
+        public static void ComputeResult()
+        {
+            if (Operation.Equals("010"))
+                Result = ValueOne + ValueTwo;
+            else if (Operation.Equals("110"))
+                Result = ValueOne - ValueOne;
+            else if (Operation.Equals("000"))
+                Result = ValueOne & ValueTwo;
+            else
+                Result = ValueOne | ValueTwo;
+        }
+
+    }
+}
---
+++ ALUControlBlock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MipsEmulator
+{
+    static class ALUControlBlock
+    {
+        public static string AluOp { get; set; }
+        public static string Function { get; set; }
+        public static string Operation { get; set; }
+
+        public static void ComputeOperation()
+        {
+            Operation = "";
+            //If lw -> add
+            if (Function.Equals("000011"))
+            {
+                Operation = "010";
+            }
+            //If R-Type -> add/sub/or/and/..
+            else
+            {
+
+                //((ALUOp1 & F1) | ALUOp0)
+                Operation += ((Convert.ToUInt32(AluOp[1].ToString(), 2) & Convert.ToUInt32(Function[1].ToString(), 2)) | Convert.ToUInt32(AluOp[0].ToString(), 2)).ToString();
+
+                //(!F2 | !ALUOp1)
+                Operation += ((1 - Convert.ToUInt32(Function[2].ToString())) | (1 - Convert.ToUInt32(AluOp[1].ToString()))).ToString();
+
+                //((F0 | F3) & ALUOp1)
+                Operation += ((Convert.ToUInt32(Function[0].ToString(), 2) | Convert.ToUInt32(Function[3].ToString(), 2)) & Convert.ToUInt32(AluOp[1].ToString(), 2)).ToString();
+
+            }
+
+        }
+    }
+}
