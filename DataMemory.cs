41dd027ef8c3dbe8c12127e987e97ee810577a39
MonicaTanios <monicatanios@gmail.com>
2020-05-27 16:20:39 +0200

    Add DataMemory & PipelineRegs Classes

---
+++ DataMemory.cs
@@ -0,0 +1,26 @@
+﻿namespace MipsEmulator
+{
+    static class DataMemory
+    {
+        public static uint Address { get; set; }
+        public static uint WriteData { get; set; }
+        public static int MemWrite { get; set; }
+        public static int MemRead { get; set; }
+        public static uint ReadData { get; set; }
+
+        public static object ComputeReadData()
+        {
+            MemWrite = ControlUnit.MemWrite;
+            MemRead = ControlUnit.MemRead;
+            ReadData = (Address < 32)
+                ? uint.Parse(MipsEmulator.MipsRegisters[string.Concat("$", Address.ToString())].ToString())
+                : 99;
+            return ReadData;
+        }
+
+        public static void ComputeWriteData()
+        {
+            MipsEmulator.MipsRegisters[string.Concat("$", Address.ToString())] = WriteData;
+        }
+    }
+}
---
+++ PipelineRegisters.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MipsEmulator
+{
+    static class PipelineRegisters
+    {
+        public static Queue<object> IFID { get; set; }
+        public static Queue<object> IDEX { get; set; }
+        public static Queue<object> EXMEM { get; set; }
+        public static Queue<object> MEMWB { get; set; }
+    }
+}
